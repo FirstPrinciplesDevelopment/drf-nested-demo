d26fd89a506164eef642a5aa7a38464166eae085
mellkior <mlmandych@roadrunner.com>
2020-12-12 15:05:09 -0500

    Added mailings app
    Scaffolded new django app and added urls.py

--- base/urls.py
+++ base/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from base.apps.mailings import urls as mailing_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(mailing_urls))
 ]
