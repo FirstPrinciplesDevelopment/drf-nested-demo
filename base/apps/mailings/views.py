9de1a1808bf2aa03ccdcb47853682021852da8c0
mellkior <mlmandych@roadrunner.com>
2021-01-20 22:16:22 -0500

    Implemented create override for MailDropViewSet

--- base/apps/mailings/views.py
+++ base/apps/mailings/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import serializers, viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
@@ -6,8 +6,34 @@ from .models import Client, MailDrop, MailRecipient
 from .serializers import (ClientSerializer, MailDropSerializer,
                           MailRecipientSerializer)
 
+# class UserViewSet(viewsets.ViewSet):
+#     """
+#     Example empty viewset demonstrating the standard
+#     actions that will be handled by a router class.
 
-class ClientViewSet(viewsets.ViewSet):
+#     If you're using format suffixes, make sure to also include
+#     the `format=None` keyword argument for each action.
+#     """
+
+#     def list(self, request):
+#         pass
+
+#     def create(self, request):
+#         pass
+
+#     def retrieve(self, request, pk=None):
+#         pass
+
+#     def update(self, request, pk=None):
+#         pass
+
+#     def partial_update(self, request, pk=None):
+#         pass
+
+#     def destroy(self, request, pk=None):
+#         pass
+
+class ClientViewSet(viewsets.ModelViewSet):
     serializer_class = ClientSerializer
     queryset = Client.objects.all()
 
@@ -24,7 +50,7 @@ class ClientViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
 
-class MailDropViewSet(viewsets.ViewSet):
+class MailDropViewSet(viewsets.ModelViewSet):
     serializer_class = MailDropSerializer
     queryset = MailDrop.objects.all()
 
@@ -40,8 +66,16 @@ class MailDropViewSet(viewsets.ViewSet):
         serializer = MailDropSerializer(maildrop, context={'request': request})
         return Response(serializer.data)
 
+    def create(self, request, *args, **kwargs):
+        maildrop = MailDrop()
+        maildrop.title = request.POST['title']
+        maildrop.client_id = int(self.kwargs['client_pk'])
+        maildrop.save()
+        serializer = MailDropSerializer(maildrop, context={'request': request})
+        return Response(serializer.data)
 
-class MailRecipientViewSet(viewsets.ViewSet):
+
+class MailRecipientViewSet(viewsets.ModelViewSet):
     serializer_class = MailRecipientSerializer
     queryset = MailRecipient.objects.all()
 
